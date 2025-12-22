37d513ba43b510a316aaf0e3588beb716b0f84c9
camiisd <liccamilasoto@gmail.com>
2025-12-22 20:27:13 -0300

    Se edita el archivo src/routes/books-routes

--- src/routes/books-routes.ts
+++ src/routes/books-routes.ts
@@ -13,3 +13,23 @@ import {
 //Importar el middleware que controla los datos de los libros
 import { validateBook } from '../middlewares/validate-middlewares'
 
+const router: Router = Router()
+
+//Rutas
+
+// 1. GET para obtener todos los libros
+router.get('/', getAllBooks)
+
+// 2. GET para obtener por ID
+router.get('/', getBookById)
+
+// 3. POST para crear libro
+router.post('/', validateBook, createBook)
+
+// 4. PATCH para actualizar libro
+router.patch('/:id', updateBook)
+
+// 5. DELETE para eliminar un libro
+router.delete('/:id', deleteBook)
+
+export default router
